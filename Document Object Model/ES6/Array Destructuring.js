ddd5413e84db058533180afe3ea380fa6a0b00dc
Md Sha Newaz <sawonnstu@gmail.com>
2024-10-28 00:06:59 +0600

    28th

---
+++ Document Object Model/ES6/Array Destructuring.js
@@ -0,0 +1,8 @@
+let fruits = ['Apple', 'Orange', 'Grape', 'Mango'];
+
+let [fruit1, fruit2, fruit3] = fruits;
+console.log(fruit1, fruit2, fruit3);
+
+// sckip anyone
+let [fruit4, , fruit5] = fruits;
+console.log(fruit4, fruit5);
---
+++ Document Object Model/ES6/Template Literals.js
@@ -0,0 +1,18 @@
+// Backtick ``
+let a = `First Line
+Second Line`;
+console.log(a);
+
+let age = 20;
+console.log(`His age is ${age}`);
+
+let name = 'Rahim';
+let dob = '20-09-2019';
+console.log(`His name is ${name} and birth is ${dob}`);
+
+let b = 34,
+c = 66;
+
+console.log(`The result is ${b+c}`);
+
+console.log(`The result ${b} + ${c} is ${b+c}`);
